244ab2594895e772bbca414d917630cea26e2cfd
e697063 <russell.petersen@sanlam.co.za>
2026-05-07 22:31:25 +0200

    Added Christi's Edits

---
+++ SomaShare/Components/Model/Dashboard.razor.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.AspNetCore.Components;
+using SomaShare.Components.Model;
+using SomaShare.Services;
+
+public partial class Dashboard : ComponentBase
+{
+    [Inject]
+    private IWantedAdService WantedAdService { get; set; } = default!;
+    [Inject]
+    private IListingService ListingService { get; set; } = default!;
+    [Inject]
+    private UserSession UserSession { get; set; } = default!;
+
+    // Models bound to the forms
+    private ListingAd newListing = new ListingAd
+    {
+        Textbook = new Textbook(),
+        User_ID = "" // will be set from session
+    };
+
+    private WantedAd newWantedAd = new WantedAd
+    {
+        Textbook = new Textbook(),
+        User_ID = "" // will be set from session
+    };
+
+    // Called when the listing form is submitted
+    private async Task CreateListing()
+    {
+        newListing.User_ID = this.UserSession.UserId;
+        await this.ListingService.CreateListingAsync(newListing.Textbook, newListing);
+        // Reset form
+        newListing = new ListingAd { Textbook = new Textbook(), User_ID = this.UserSession.UserId };
+    }
+
+    // Called when the wanted ad form is submitted
+    private async Task CreateWantedAd()
+    {
+        newWantedAd.User_ID = this.UserSession.UserId;
+        await this.WantedAdService.CreateWantedAdAsync(newWantedAd.Textbook, newWantedAd);
+        // Reset form
+        newWantedAd = new WantedAd { Textbook = new Textbook(), User_ID = this.UserSession.UserId };
+    }
+}
